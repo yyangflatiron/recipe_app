8fd092b6ac67a1796782520cc8eca8b266aadf39
Dan <dandanberry@gmail.com>
2016-07-12 20:14:22 -0400

    everything but the flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,15 @@ class SessionsController < ApplicationController
 
   def create
   	@user = User.find_by(name: params[:name])
-    flash.now[:error] = "try again" unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
+		if @user.authenticate(params[:password])
+		    session[:user_id] = @user.id
+		    redirect_to user_path(@user)
+		else 
+			flash[:message] = "try again"
+	        redirect_to '/sign_in' 
+	    end
+
+    # flash.now[:message] = "try again" 
 
     # return head(:forbidden) unless @user.authenticate(params[:password])
 
