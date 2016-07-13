15bb811ab70022d37f6a6eedec985bfb1b91e3d2
abillig <avrambillig@gmail.com>
2016-07-13 09:47:35 -0400

    create flash error message for invalid name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(name: params[:name])
-		if @user.authenticate(params[:password])
-		    session[:user_id] = @user.id
-		    redirect_to user_path(@user)
-		else 
-			flash[:message] = "try again"
-	        redirect_to '/sign_in' 
-	    end
+  	if @user = User.find_by(name: params[:name])
+  		if @user.authenticate(params[:password])
+  		    session[:user_id] = @user.id
+  		    redirect_to user_path(@user)
+  		else 
+  			flash[:message] = "try again"
+  	    redirect_to '/sign_in' 
+  	  end
+    else 
+      flash[:message] = "who are you?"
+        redirect_to '/sign_in' 
+    end
   end
-
   
+
+
 end
