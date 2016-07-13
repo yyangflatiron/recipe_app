faff21b384d5d6b65ba1a00ee37fcc4cd50789f2
abillig <avrambillig@gmail.com>
2016-07-13 11:46:32 -0400

    began new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,20 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show 
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new 
+    @recipe = Recipe.new
+  end
+
+  def create
+
+  end
+
+
 end
