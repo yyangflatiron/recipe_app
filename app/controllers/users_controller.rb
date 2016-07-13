8f24ab82c4fa978ddc6bb8f17d833e9aa2b24051
Kellye Greene <kellye@gust.com>
2016-07-12 20:08:03 -0400

    creates sign up flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, 
+                                 :fav_cuisine, 
+                                 :bio,
+                                 :image,
+                                 :allergies,
+                                 :hometown,
+                                 :email,
+                                 :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-	has_one :author
-	has_one :reader
+  has_secure_password
+  # validates :name, presence: true
+  
+  has_one :author
+  has_one :reader
+
 end
